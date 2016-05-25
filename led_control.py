8f1b7cb8db8317110da770b6c81c057c3c3ed8f9
Brian Lin <ys.ncku@gmail.com>
2016-05-25 08:41:11 +0800

    Modify.

--- led_control.py
+++ led_control.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from ubidots import ApiClient
 import time
 
+
 # Init GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
@@ -11,16 +12,16 @@ api = ApiClient(token='BQVCK7vEK1t59qPNFGVgCdyZ8HRogc')
 
 # Get a Ubidots Variable
 try:
-    statusVar = api.get_variable("573fbb1f7625423b1d1c0523")
+    stateVar = api.get_variable("573fbb1f7625423b1d1c0523")
 except ValueError:
     print "It is not possible to obtain the variable"
 
 while(1):
     try:
         # Get the latest value.
-        status = statusVar.get_values(1)
-        v = status[0]['value']
-        print "Got status %d at %s" % (v, status[0]['created_at'])
+        state = stateVar.get_values(1)
+        v = state[0]['value']
+        print "Got state %d at %s" % (v, state[0]['created_at'])
         if v == 1:
             GPIO.output(11, GPIO.HIGH)
         else:
